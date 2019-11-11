b0e3fc4f3ff29fc054799d93fdb13b6bc6c185d4
tommybru <tbru@stanford.edu>
2019-11-11 01:02:09 -0800

    Adding app code and images

---
+++ game.java
@@ -0,0 +1,561 @@
+public class game {
+
+	static boolean gameover = false;
+	static String p1name;
+	static String p2name;
+	static tank[] p1tanks = new tank[3];
+	static tank[] p2tanks = new tank[3];
+	static boolean p1turn = false;
+	static String answer;
+	static int answerInt;
+	static int diceRolled;
+	static int randomNum;
+	static tank attackingTank;
+	static int attackTotal;
+	static tank defendingTank;
+	static int healTotal;
+	static int time;
+	static boolean attacked = false;
+
+
+
+	public static void main(String[] args) {
+		welcome();
+		picking();
+		while(!gameover) {
+			System.err.flush();
+			System.out.flush();
+			attack();
+			if(attacked && defendingTank.health > 0 && defendingTank.staff > 0) {
+				sleep(time = 3000);
+				System.err.flush();
+				System.out.flush();
+				defend();
+			}
+			else {
+				sleep(time = 1000);
+				System.out.println(" ");
+				System.out.println("There was no attack or the Defending Tank's health or staff is 0 or below. There will be no defense.");
+				sleep(time = 100);
+			}
+			if(p1turn) {
+				p1turn = false;
+			}
+			else {
+				p1turn = true;
+			}
+			checkP1();
+			checkP2();
+			if(checkP1() || checkP2()) {
+				gameover = true;
+			}
+			sleep(time = 5000);
+		}
+
+		System.out.println(" ");
+		sleep(time = 700);
+		System.err.print("WINNER");
+		sleep(time = 1);
+
+		for(int i =0;i<6;i++) {
+			sleep(time = 250);
+			System.out.print(".");
+
+		}
+		sleep(time = 200);
+		if(checkP1()){
+			System.err.print(p2name);
+		}
+		else {
+			System.err.print(p1name);
+		}
+		sleep(time = 1);
+		System.out.print(" has ANNIHILATED ");
+		if(checkP1()){
+			System.err.print(p1name);
+		}
+		else {
+			System.err.print(p2name);
+		}
+		sleep(time = 4);
+		System.out.println(" ");
+		System.out.println("CONGRATS! GAME OVER!");
+
+
+	}
+
+	public static void welcome() {
+		sleep(time = 200);
+		Scanner theInput = new Scanner(System.in);
+		System.err.print("WELCOME TO RISK");
+		for(int i =0;i<6;i++) {
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.err.print(".");
+
+		}
+		System.out.println(" ");
+		System.out.println(" ");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Player 1, what is your name?");
+		p1name = theInput.nextLine();
+		System.out.println("Player 2, what is your name?");
+		p2name = theInput.nextLine();
+
+		System.out.println(" ");
+		System.out.println("Here are your tank options:");
+		tank exattacker = new tank();
+		exattacker.attacker = true;
+		tank exhealer = new tank();
+		exhealer.healer = true;
+		tank exarmor = new tank();
+		exarmor.wellarmor = true;
+		tank[] exampleTanks = {exattacker, exhealer, exarmor};
+
+
+		for(int i=0;i<3;i++) {
+			System.out.println(" ");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			exampleTanks[i].setup();
+			System.err.println(exampleTanks[i].type);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			exampleTanks[i].printAllInfo();
+
+		}
+	}
+
+	public static void picking() {
+		Scanner theInput = new Scanner(System.in);
+		System.out.println(" ");
+		System.out.println(" ");
+		sleep(time = 1000);
+		System.err.println("DRAFTING");
+		sleep(time = 500);
+		System.out.println("Type 1 is Attacker");
+		System.out.println("Type 2 is Healer");
+		System.out.println("Type 3 is Well-Armored");
+		System.out.println(" ");
+		sleep(time = 1000);
+		for(int i=0;i<3;i++) {
+			System.err.print(p1name);
+			System.out.print(" pick the type of your Tank #" + (i+1));
+			System.out.println(" ");
+			answerInt = Integer.parseInt(theInput.nextLine());
+			while((answerInt != 1) && (answerInt != 2) && (answerInt !=3)) {
+				System.out.println("Please Pick: 1, 2, or 3");
+				System.err.print("");
+				System.out.println("1 for Attacker, 2 for Healer, or 3 for Well-Armored");
+				answerInt = Integer.parseInt(theInput.nextLine());
+			}
+			p1tanks[i] = new tank();
+			if(answerInt == 1) {
+				p1tanks[i].attacker = true;
+			}
+			if(answerInt == 2) {
+				p1tanks[i].healer = true;
+			}
+			if(answerInt == 3) {
+				p1tanks[i].wellarmor = true;
+			}
+			p1tanks[i].setup();
+
+
+			System.err.print(p2name);
+			System.out.print(" pick the type of your Tank #" + (i+1));
+			System.out.println(" ");
+			answerInt = Integer.parseInt(theInput.nextLine());
+			while((answerInt != 1) && (answerInt != 2) && (answerInt !=3)) {
+				System.out.println("Please Pick: 1, 2, or 3");
+				System.err.print("");
+				System.out.println("1 for Attacker, 2 for Healer, or 3 for Well-Armored");
+				answerInt = Integer.parseInt(theInput.nextLine());
+			}
+			p2tanks[i] = new tank();
+			if(answerInt == 1) {
+				p2tanks[i].attacker = true;
+			}
+			if(answerInt == 2) {
+				p2tanks[i].healer = true;
+			}
+			if(answerInt == 3) {
+				p2tanks[i].wellarmor = true;
+			}
+			p2tanks[i].setup();
+
+
+			randomNum = (int)(Math.random()*2);
+			if(randomNum == 0) {
+				p1turn = true;
+			}
+			else {
+				p1turn = false;
+			}
+		}
+	}
+
+	public static void attack() {
+		Scanner theInput = new Scanner(System.in);
+		System.out.println(" ");
+		sleep(time = 700);
+		System.err.print("ATTACKING");
+		sleep(time = 1);
+
+		for(int i =0;i<6;i++) {
+			sleep(time = 250);
+			System.out.print(".");
+
+		}
+		System.out.println(" ");
+		if(p1turn) {
+			System.err.println("It is " + p1name +"'s turn");
+			System.out.println(" ");
+			sleep(time = 1);
+			System.out.println(p1name +", would you like to attack? Yes or No");
+			answer = theInput.nextLine();
+			if(answer.equals("Yes") || answer.equals("yes")) {
+				attacked = true;
+				System.out.println("Which tank would you like to use to attack?");
+				for(int i=0;i<3;i++) {
+					System.out.println(" ");
+					sleep(time = 700);
+					System.err.println("Tank #"+(i+1));
+					if(p1tanks[i].health>0) {
+						sleep(time=5);
+						System.err.println(p1tanks[i].type);
+						sleep(time=100);
+						p1tanks[i].printAllInfo();
+					}
+					else {
+						sleep(time=5);
+						System.out.println("DEAD");
+					}
+
+				}
+				answerInt = Integer.parseInt(theInput.nextLine());
+				while(((answerInt != 1) && (answerInt != 2) && (answerInt !=3)) || p1tanks[answerInt-1].health<=0  || p1tanks[answerInt-1].staff<=0) {
+					System.out.println("Please Pick: 1, 2, or 3");
+					System.err.print("");
+					System.out.println("1 for Tank #1, 2 for Tank #2, or 3 for Tank #3");
+					System.out.println("Also make sure you are selecting a tank that has health and staff left.");
+					answerInt = Integer.parseInt(theInput.nextLine());
+				}
+				sleep(time = 40);
+				System.err.print(p1name);
+				System.out.print(" is now attacking with tank #" + answerInt);
+				System.out.println(" ");
+				attackingTank = p1tanks[answerInt-1];
+				sleep(time = 400);
+				randomNum = (int)(Math.random()*3);
+				while(p2tanks[randomNum].health <= 0) {
+					randomNum = (int)(Math.random()*3);
+				}
+				sleep(time = 1000);
+				System.err.print(p2name);
+				System.out.println(" will be defending with randomly selected tank #" + (randomNum+1));
+				System.out.println(" ");
+				sleep(time = 2);
+				defendingTank = p2tanks[randomNum];
+				System.err.println("Tank #"+(randomNum + 1));
+				sleep(time = 500);
+				System.err.println(p2tanks[randomNum].type);
+				sleep(time = 100);
+				p2tanks[randomNum].printAllInfo();
+				sleep(time = 300);
+				System.out.println(" ");
+				System.out.println(" ");
+				sleep(time = 2);
+				System.err.print(p1name);
+				System.out.println(", how many dice will you roll? (Maximum of 6) (Staff of tank #"+answerInt+" will be reduced by the amount of dice you roll)");
+				diceRolled = Integer.parseInt(theInput.nextLine());
+				while( ( (diceRolled != 1) && (diceRolled != 2) && (diceRolled !=3)  && (diceRolled !=4)  && (diceRolled !=5)  && (diceRolled !=6) ) || diceRolled>attackingTank.staff) {
+					System.out.println("Please Pick: 1, 2, 3, 4, 5, or 6 dice to roll");
+					System.out.println("Make sure you have enough staff for the amount of dice as well");
+					diceRolled = Integer.parseInt(theInput.nextLine());
+				}
+				int[] diceRolls;
+				attackTotal = attackingTank.printAndAddDice(diceRolls = attackingTank.rollDice(diceRolled, 6));
+				System.out.println(" ");
+				sleep(time = 1);
+				if(!attackingTank.actuallyAttack(attackTotal = attackTotal, attackingTank = attackingTank, defendingTank = defendingTank, p1turn = p1turn, p1name = p1name, p2name = p2name)) {
+					attacked = false;
+				}
+				attackingTank.redoAttackingStaff(diceRolled = diceRolled, attackingTank = attackingTank, p1turn = p1turn, p1name = p1name, p2name = p2name);
+
+				for(int i=0;i<3;i++) {
+					System.out.println(" ");
+					sleep(time = 700);
+					System.err.println("Tank #"+(i+1));
+					sleep(time=5);
+					if(p1tanks[i].health>0) {
+						System.err.println(p1tanks[i].type);
+						sleep(time=100);
+						if(i!=(answerInt-1)) {
+							p1tanks[i].staff = p1tanks[i].staff + 1;
+							p1tanks[i].health = p1tanks[i].health + 5;
+						}
+						p1tanks[i].printAllInfo();
+					}
+					else {
+						System.out.println("DEAD");
+					}
+
+				}
+			}
+			else {
+				attacked = false;
+				System.out.println("Okay, all alive tanks get +2 staff and +10 health");
+				sleep(time = 1000);
+				System.err.print(p1name);
+				System.out.println(", here are your tanks current attributes:");
+				sleep(time = 1);
+				for(int i=0;i<3;i++) {
+					System.out.println(" ");
+					sleep(time = 700);
+					System.err.println("Tank #"+(i+1));
+					sleep(time=5);
+					if(p1tanks[i].health>0) {
+						System.err.println(p1tanks[i].type);
+						sleep(time=100);
+						p1tanks[i].staff = p1tanks[i].staff + 2;
+						p1tanks[i].health = p1tanks[i].health + 10;
+						p1tanks[i].printAllInfo();
+					}
+					else {
+						System.out.println("DEAD");
+					}
+
+				}
+			}
+		}
+		else {
+			System.err.println("It is " + p2name +"'s turn");
+			System.out.println(" ");
+			sleep(time = 1);
+			System.out.println(p2name +", would you like to attack? Yes or No");
+			answer = theInput.nextLine();
+			if(answer.equals("Yes") || answer.equals("yes")) {
+				attacked = true;
+				System.out.println("Which tank would you like to use to attack?");
+				for(int i=0;i<3;i++) {
+					System.out.println(" ");
+					sleep(time = 700);
+					System.err.println("Tank #"+(i+1));
+					sleep(time=5);
+					if(p2tanks[i].health>0) {
+						System.err.println(p2tanks[i].type);
+						sleep(time=100);
+						p2tanks[i].printAllInfo();
+					}
+					else {
+						System.out.println("DEAD");
+					}
+
+				}
+				answerInt = Integer.parseInt(theInput.nextLine());
+				while(((answerInt != 1) && (answerInt != 2) && (answerInt !=3)) || p2tanks[answerInt-1].health<=0 || p2tanks[answerInt-1].staff<=0) {
+					System.out.println("Please Pick: 1, 2, or 3");
+					System.err.print("");
+					System.out.println("1 for Tank #1, 2 for Tank #2, or 3 for Tank #3");
+					System.out.println("Also make sure you are selecting a tank that has health and staff left.");
+					answerInt = Integer.parseInt(theInput.nextLine());
+				}
+				sleep(time = 4);
+				System.err.print(p2name);
+				System.out.print(" is now attacking with tank #" + answerInt);
+				System.out.println(" ");
+				attackingTank = p2tanks[answerInt-1];
+				sleep(time = 400);
+				randomNum = (int)(Math.random()*3);
+				while(p1tanks[randomNum].health <= 0) {
+					randomNum = (int)(Math.random()*3);
+				}
+				sleep(time = 1000);
+				System.err.print(p1name);
+				System.out.println(" will be defending with randomly selected tank #" + (randomNum+1));
+				System.out.println(" ");
+				sleep(time = 2);
+				defendingTank = p1tanks[randomNum];
+				System.err.println("Tank #"+(randomNum + 1));
+				sleep(time = 500);
+				System.err.println(p1tanks[randomNum].type);
+				sleep(time = 100);
+				p1tanks[randomNum].printAllInfo();
+				sleep(time = 300);
+				System.out.println(" ");
+				System.out.println(" ");
+				sleep(time = 1);
+				System.err.print(p2name);
+				System.out.println(", how many dice will you roll? (Maximum of 6) (Staff of tank #"+answerInt+" will be reduced by the amount of dice you roll)");
+				diceRolled = Integer.parseInt(theInput.nextLine());
+				while(((diceRolled != 1) && (diceRolled != 2) && (diceRolled !=3)  && (diceRolled !=4)  && (diceRolled !=5) && (diceRolled !=6)) || diceRolled>p2tanks[answerInt-1].staff) {
+					System.out.println("Please Pick: 1, 2, 3, 4, 5, or 6 dice to roll");
+					System.out.println("Make sure you have enough staff for that dice roll as well");
+					diceRolled = Integer.parseInt(theInput.nextLine());
+				}
+				int[] diceRolls;
+				attackTotal = attackingTank.printAndAddDice(diceRolls = attackingTank.rollDice(diceRolled, 6));
+				System.out.println(" ");
+				sleep(time = 1);
+				if(!attackingTank.actuallyAttack(attackTotal = attackTotal, attackingTank = attackingTank, defendingTank = defendingTank, p1turn = p1turn, p1name = p1name, p2name = p2name)) {
+					attacked = false;
+				}
+				attackingTank.redoAttackingStaff(diceRolled = diceRolled, attackingTank = attackingTank, p1turn = p1turn, p1name = p1name, p2name = p2name);
+
+				for(int i=0;i<3;i++) {
+					System.out.println(" ");
+					sleep(time = 700);
+					System.err.println("Tank #"+(i+1));
+					sleep(time=5);
+					if(p2tanks[i].health>0) {
+						System.err.println(p2tanks[i].type);
+						sleep(time=100);
+						if(i!=(answerInt-1)) {
+							p2tanks[i].staff = p2tanks[i].staff + 1;
+							p2tanks[i].health = p2tanks[i].health + 5;
+						}
+						p2tanks[i].printAllInfo();
+					}
+					else {
+						System.out.println("DEAD");
+					}
+
+				}
+			}
+			else {
+				attacked = false;
+				System.out.println("Okay, all alive tanks get +2 staff and +10 health");
+				sleep(time = 1000);
+				System.err.print(p2name);
+				System.out.println(", here are your tanks current attributes:");
+				sleep(time = 1);
+				for(int i=0;i<3;i++) {
+					System.out.println(" ");
+					sleep(time = 700);
+					System.err.println("Tank #"+(i+1));
+					sleep(time=5);
+					if(p2tanks[i].health>0) {
+						System.err.println(p2tanks[i].type);
+						sleep(time=100);
+						p2tanks[i].staff = p2tanks[i].staff + 2;
+						p2tanks[i].health = p2tanks[i].health + 10;
+						p2tanks[i].printAllInfo();
+					}
+					else {
+						System.out.println("DEAD");
+					}
+
+				}
+			}
+		}
+
+	}
+
+	public static void defend() {
+		Scanner theInput = new Scanner(System.in);
+		System.out.println(" ");
+		sleep(time = 700);
+		System.err.print("DEFENDING");
+		sleep(time = 1);
+
+		for(int i =0;i<6;i++) {
+			sleep(time = 250);
+			System.out.print(".");
+
+		}
+		System.out.print(" ");
+		sleep(time = 25);
+		if(p1turn) {
+			System.err.print(p2name);
+		}
+		else {
+			System.err.print(p1name);
+		}
+		sleep(time = 1);
+		System.out.println(", you can now recover. Here are the attributes of your tank that was just attacked: ");
+
+		System.out.println(" ");
+		sleep(time = 700);
+		System.err.println("Tank #"+(randomNum+1));
+		sleep(time=5);
+		System.err.println(defendingTank.type);
+		sleep(time=100);
+		defendingTank.printAllInfo();
+
+		System.out.println("Your tank has a resilience of " + defendingTank.resilience + ", would you like to recover it? Yes or No.");
+		answer = theInput.nextLine();
+		if(answer.equals("Yes") || answer.equals("yes")) {
+			System.out.println("How many dice would you like to roll? (Maximum is " + defendingTank.resilience + ") (The staff of the tank will be reduced by how many dice you roll)");
+			diceRolled = Integer.parseInt(theInput.nextLine());
+			while( (diceRolled <= 0) || (diceRolled > defendingTank.resilience) || diceRolled>defendingTank.staff) {
+				System.out.println("Please pick a number of dice between 1 and " + defendingTank.resilience);
+				System.out.println("Make sure you have enough staff for that dice roll as well");
+				diceRolled = Integer.parseInt(theInput.nextLine());
+			}
+			int[] diceRolls;
+			healTotal = defendingTank.printAndAddDice(diceRolls = defendingTank.rollDice(diceRolled, 6));
+			defendingTank.actuallyDefend(defendingTank = defendingTank, healTotal = healTotal);
+			System.out.println(" ");
+			defendingTank.redoDefendingStaff(diceRolled = diceRolled, defendingTank = defendingTank, p1turn = p1turn, p1name = p1name, p2name = p2name);
+			sleep(time = 1);
+			System.err.println("Tank #"+(randomNum + 1));
+			sleep(time = 500);
+			System.err.println(defendingTank.type);
+			sleep(time = 100);
+			defendingTank.printAllInfo();
+			sleep(time = 300);
+			System.out.println(" ");
+			System.out.println(" ");
+
+
+		}
+		else {
+			System.out.println("Okay.");
+			sleep(time = 400);
+		}
+	}
+
+	public static boolean checkP1() {
+		if( p1tanks[0].isDead() && p1tanks[1].isDead() && p1tanks[2].isDead() ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public static boolean checkP2() {
+		if( p2tanks[0].isDead() && p2tanks[1].isDead() && p2tanks[2].isDead() ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public static void sleep(int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.err.flush();
+		System.out.flush();
+	}
+
+
+}
---
+++ sprite.java
@@ -0,0 +1,264 @@
+import java.util.Scanner;
+
+
+public class	sprite	{
+	int	power;
+	int	armor;
+	int	resilience;
+	int	health;
+	int	staff;
+
+	public	sprite()	{
+		power	=	10;
+		resilience	=	10;
+		armor	=	20;
+		health	=	300;
+		staff	=	30;
+	}
+
+	public	sprite(int	somePower,	int	someResilience,	int	someArmor,	int	someHealth,	int	someStaff)	{
+		power	=	somePower;
+		resilience	=	someResilience;
+		armor	=	someArmor;
+		health	=	someHealth;
+		staff	=	someStaff;
+	}
+
+	public	void	printAllInfo()	{
+		System.out.println("Power: "+power);
+		System.out.println("Resilience: "+resilience);
+		System.out.println("Armor: "+armor);
+		System.out.println("Health: "+health);
+		System.out.println("Staff: "+staff);
+	}
+
+	private	int	rollOneDie(int	range,	int	low)	{
+		int	result	=	(int)(Math.random()*range)+low;
+		return	result;
+	}
+
+	public	int[]	rollDice(int	howMany,	int	numSides)	{
+		int[]	results;
+		results	=	new	int[howMany];
+		for(int	i=0;	i<howMany;	i++)	{
+			results[i]	=	rollOneDie(numSides,	1);
+		}
+
+		return	results;
+	}
+
+	public	boolean	isDead()	{
+		if(health<=0)	{
+			return	true;
+		}
+		else	{
+			return	false;
+		}
+	}
+
+	public	boolean	canAttack(sprite	other)	{
+		if(health>other.health)	{
+			return	true;
+		}
+		else	{
+			return	false;
+		}
+	}
+}
+
+class tank extends sprite {
+	boolean attacker;
+	boolean healer;
+	boolean wellarmor;
+	String type;
+	static int time;
+
+	public void setup() {
+		if(attacker) {
+			type = "Attacker";
+			power = 12;
+			resilience = 3;
+			armor = 7;
+			health = 125;
+			staff = 14;
+		}
+		else if(healer) {
+			type = "Healer";
+			power = 7;
+			resilience = 4;
+			armor = 6;
+			health = 150;
+			staff = 14;
+		}
+		else if(wellarmor) {
+			type = "Well-Armored";
+			power = 6;
+			resilience = 2;
+			armor = 13;
+			health = 100;
+			staff = 12;
+		}
+	}
+
+
+	static int printAndAddDice(int[] diceRolls) {
+		int sum = 0;
+		sleep(time = 500);
+		System.out.print("Your dice rolls are: ");
+		for(int i=0;i<diceRolls.length;i++) {
+			sleep(time = 500);
+			System.out.print(diceRolls[i] + ", ");
+			sum+=diceRolls[i];
+		}
+		sleep(time = 500);
+		System.out.print("totaling " + sum);
+		return sum;
+
+	}
+
+	static boolean actuallyAttack(int attackTotal, tank attackingTank, tank defendingTank, boolean p1turn, String p1name, String p2name) {
+		sleep(time = 1500);
+		if(p1turn) {
+			System.err.print(p2name);
+		}
+		else {
+			System.err.print(p1name);
+		}
+		sleep(time = 1);
+		System.out.print("'s defending tank has armor of " + defendingTank.armor);
+		System.out.println(" ");
+		System.out.println(attackTotal + " - " + defendingTank.armor + " = " + (attackTotal - defendingTank.armor));
+		if((attackTotal - defendingTank.armor)<=0) {
+			sleep(time = 2000);
+			System.out.println("Because there is no damage done, attack is now void.");
+			return false;
+		}
+		else {
+			sleep(time = 2000);
+			if(p1turn) {
+				System.err.print(p1name);
+			}
+			else {
+				System.err.print(p2name);
+			}
+			sleep(time = 1);
+			System.out.print("'s attacking tank has power of " + attackingTank.power);
+			System.out.println(" ");
+			System.out.println( (attackTotal - defendingTank.armor)  + " x " + attackingTank.power + " = " + ( (attackTotal - defendingTank.armor)  * attackingTank.power));
+		}
+		attackTotal = (attackTotal - defendingTank.armor) * attackingTank.power;
+		if(attackTotal>0) {
+			sleep(time = 2000);
+			if(p1turn) {
+				System.err.print(p2name);
+			}
+			else {
+				System.err.print(p1name);
+			}
+			sleep(time = 1);
+			System.out.print("'s defending tank started with health of " + defendingTank.health);
+			System.out.println(" ");
+			System.out.println(defendingTank.health + " - " + (attackTotal) + " = " + (defendingTank.health - attackTotal));
+			defendingTank.health = defendingTank.health - attackTotal;
+		}
+		return true;
+	}
+
+	public void actuallyDefend(tank defendingTank, int healTotal) {
+		sleep(time = 1000);
+		System.out.println(" ");
+		if(defendingTank.staff > 1) {
+			System.out.print("Staff/2 is " + (defendingTank.staff/2) + ". ");
+			sleep(time = 1000);
+			System.out.print(healTotal + " x " +  (defendingTank.staff/2) + " = " + (healTotal * (defendingTank.staff/2)) + ".");
+			sleep(time = 1000);
+			System.out.println(" ");
+			System.out.println("Health is now: " + defendingTank.health + " + " + (healTotal * (defendingTank.staff/2)));
+			defendingTank.health = defendingTank.health + (healTotal * (defendingTank.staff/2));
+		}
+		else {
+			sleep(time = 1000);
+			System.out.print(healTotal + " x 1 = " + (healTotal) + ".");
+			sleep(time = 1000);
+			System.out.println(" ");
+			System.out.println("Health is now: " + defendingTank.health + " + " + healTotal);
+			defendingTank.health = defendingTank.health + (healTotal);
+		}
+	}
+
+	public void redoAttackingStaff(int diceRolled, tank attackingTank, boolean p1turn, String p1name, String p2name) {
+		sleep(time = 2500);
+		if(p1turn) {
+			System.err.print(p1name);
+		}
+		else {
+			System.err.print(p2name);
+		}
+		sleep(time = 1);
+		System.out.print(", your attacking tank started with staff of " + attackingTank.staff + ". ");
+		if(diceRolled != 1)	{
+			System.out.print("You rolled " + diceRolled + " dice. ");
+		}
+		else {
+			System.out.print("You rolled " + diceRolled + " die. ");
+		};
+		System.out.println(attackingTank.staff + " - " + (diceRolled) + " = " + (attackingTank.staff - diceRolled));
+		attackingTank.staff = attackingTank.staff - diceRolled;
+		sleep(time = 2000);
+		if(p1turn) {
+			System.err.print(p1name);
+		}
+		else {
+			System.err.print(p2name);
+		}
+		sleep(time = 1);
+		System.out.print(", your remaining alive tanks received +1 staff and +5 health");
+		System.out.println(" ");
+		sleep(time = 5000);
+		System.out.println("Here are your tanks' current attributes: ");
+
+	}
+
+	public void redoDefendingStaff(int diceRolled, tank defendingTank, boolean p1turn, String p1name, String p2name) {
+		sleep(time = 2500);
+		if(p1turn) {
+			System.err.print(p2name);
+		}
+		else {
+			System.err.print(p1name);
+		}
+		sleep(time = 1);
+		System.out.print(", your defending tank started with staff of " + defendingTank.staff + ". ");
+		if(diceRolled != 1)	{
+			System.out.print("You rolled " + diceRolled + " dice. ");
+		}
+		else {
+			System.out.print("You rolled " + diceRolled + " die. ");
+		}
+		System.out.println(defendingTank.staff + " - " + diceRolled + " = " + (defendingTank.staff - diceRolled));
+		defendingTank.staff = defendingTank.staff - diceRolled;
+		sleep(time = 6500);
+		if(p1turn) {
+			System.err.print(p2name);
+		}
+		else {
+			System.err.print(p1name);
+		}
+		sleep(time = 1);
+		System.out.print(", here is your defending tank's attributes after healing: ");
+		sleep(time = 1);
+		System.out.println(" ");
+		System.out.println(" ");
+
+	}
+
+	private static void sleep(int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.err.flush();
+		System.out.flush();
+	}
+}
